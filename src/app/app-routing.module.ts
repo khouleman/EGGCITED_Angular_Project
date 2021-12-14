410811c0c4635c850c097d130bd4bc89f24486a7
Nathan DEMURU <blakei.57@icloud.com>
2021-12-14 17:16:12 +0100

    fin du projet, optimisation du code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {FilmListComponent} from "./film-list/film-list.component";
 import {FoodNewComponent} from "./food-new/food-new.component";
 import {HomeComponent} from "./home/home.component";
-import {AppComponent} from "./app.component";
 import {FoodModifComponent} from "./food-modif/food-modif.component";
-import {BrowserModule} from "@angular/platform-browser";
-import {FormsModule} from "@angular/forms";
-import {FoodComponent} from "./food/food.component";
 import {FoodListComponent} from "./food-list/food-list.component";
 
 const routes: Routes = [
-  {
-    path:"films",
-    component: FilmListComponent
-  },
   {
     path:"food",
     component: FoodListComponent
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FilmComponent } from './film/film.component';
 import { FormsModule } from "@angular/forms";
-import {FilmService} from "./services/film/film.service";
-import { FilmListComponent } from './film-list/film-list.component';
-import { FilmModifComponent } from './film-modif/film-modif.component';
-import { FilmNewComponent } from './film-new/film-new.component';
 import { HomeComponent } from './home/home.component';
 import {AngularFireModule} from "@angular/fire";
 import {AngularFirestoreModule} from "@angular/fire/firestore";
@@ -22,10 +17,6 @@ import {FoodService} from "./services/food/food.service";
 @NgModule({
   declarations: [
     AppComponent,
-    FilmComponent,
-    FilmListComponent,
-    FilmModifComponent,
-    FilmNewComponent,
 
     HomeComponent,
 
